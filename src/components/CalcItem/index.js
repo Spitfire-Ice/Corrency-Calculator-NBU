b970fa31a7d9a0df72eca53944402bf1d5eb21f4
Spitfire-Ice <37056025+Spitfire-Ice@users.noreply.github.com>
2020-03-06 00:59:59 +0200

    layout design, add-remove logic

---
+++ src/components/CalcItem/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+
+
+import "./style.scss";
+
+
+const currencies = [
+  {
+    value: 'UAH',
+    label: '₴',
+  },
+  {
+    value: 'USD',
+    label: '$',
+  },
+  {
+    value: 'EUR',
+    label: '€',
+  },
+  {
+    value: 'JPY',
+    label: '¥',
+  },
+];
+
+const CalcItem = ({calculators,... props}) => {
+  const [currency, setCurrency] = React.useState('UAH');
+
+  const handleChange = event => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <div className="CalcItem">
+      <TextField
+        id="outlined-number"
+        label="Value"
+        type="number"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+      />
+      <TextField
+        id="standard-select-currency"
+        select
+        label="Select"
+        value={currency}
+        onChange={handleChange}
+        helperText="Please select your currency"
+      >
+        {currencies.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <IconButton
+        aria-label="delete"
+        onClick={props.deleteCalculator}
+      >
+        <DeleteIcon />
+      </IconButton>
+    </div>
+  )
+};
+
+export default CalcItem
--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,59 @@
 import React, { Component } from "react";
-import './app.scss';
+import CalcItem from "./CalcItem";
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from "@material-ui/core/IconButton";
 
+
+import './app.scss';
 
 
 class App extends Component {
   state = {
-
+    calculators: [
+      { id: 0, value: "UAH" }
+    ]
   };
 
-  render() {
+  addCalculator = () => {
+    this.setState( state => {
+      state.calculators.push({
+        id: state.calculators.length !== 0 ? state.calculators.length : 0,
+        value: "UAH",
+      });
+      return state
+    })
+  };
+  deleteCalculator = id => {
+    const index = this.state.calculators.map( calc => calc.id).indexOf(id);
+    this.setState(state => {
+      delete state.calculators[index];
+      return state
+    });
+  };
 
+
+  render() {
+    const {calculators} = this.state;
+
+    return (
+      <div className="app--container">
+        <h1>Currency Calculator</h1>
+        {calculators.map(calculator =>(
+          <CalcItem
+            key={calculator.id}
+            calculator={calculator}
+            deleteCalculator={()=>this.deleteCalculator(calculator.id)}
+          />
+        ))}
+        <div className="addButton">
+          <IconButton aria-label="add"  onClick={this.addCalculator}>
+              <AddIcon />
+          </IconButton>
+        </div>
+      </div>
+      )
   }
 }
+console.log('hello');
 
 export default App;
